e8e8e2a95a48263892048499c4ec771655f7709d
JOYERUGO-STUDENT Lab5 <g00435295@atu.ie>
2024-10-23 17:32:24 +0100

    Code1

--- src/main/java/org/example/TryExample.java
+++ src/main/java/org/example/TryExample.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class TryExample {
     public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Please enter a number between 0 - 9 :");
 
-        System.out.println("Please enter a number between 0 - 9:");
-        Scanner myscan = new Scanner(System.in);
+                String input = scan.nextLine();
 
-        // Read User input as a string
-        String input = myscan.nextLine();
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+                try {
+                    // Parse the input to an integer
+                    int myNum = Integer.parseInt(input);
 
-        //Check if the number is between 0 and 9
-        if (myNum >= 0 && myNum <= 9) {
-            System.out.println("You entered " + myNum);
+                    // Check if the number is between 0 and 9
+                    if (myNum >= 0 && myNum <= 9) {
+                        System.out.println("You entered " + myNum);
+                        break;
+                    } else {
+                        System.out.println("Not a valid number. Please enter a number between 0 and 9.");
+                    }
+
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Not a valid number, try again!");
+                }
+            }
         }
-        else {
-            System.out.println("Not a valid number");
-        }
-
-
     }
 }
-
